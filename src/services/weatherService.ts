2277c92cf714bedc143a8a36ae9333140cc4e4dd
volkanb <volkanbenli92@gmail.com>
2024-11-05 23:49:46 -0500

    Remove exposed key

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -1,6 +1,6 @@
 // src/services/weatherService.ts
 
-const API_KEY = '311ef8bfb36d1f2557b8c79ddd3ebabc';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export interface WeatherData {
